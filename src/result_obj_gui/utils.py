99f4096a8a0429a35a23068343316cef6e550da4
Bystroushaak <bystrousak@kitakitsune.org>
2022-02-08 04:49:04 +0100

    Added new util function iso_str_from_ts().

--- src/result_obj_gui/utils.py
+++ src/result_obj_gui/utils.py
@@ -6,14 +6,21 @@ LOCAL_TIMEZONE = datetime.now(timezone.utc).astimezone().tzinfo
 
 
 def str_from_ts(ts):
-    dt_object = datetime.fromtimestamp(ts)
+    dt_object = datetime.fromtimestamp(float(ts))
     local_tz_dt = dt_object.astimezone(LOCAL_TIMEZONE)
 
     return local_tz_dt.strftime("%Y-%m-%d %H:%M:%S")
 
 
+def iso_str_from_ts(ts):
+    dt_object = datetime.fromtimestamp(float(ts))
+    local_tz_dt = dt_object.astimezone(LOCAL_TIMEZONE)
+
+    return local_tz_dt.isoformat()
+
+
 def html_from_ts(ts):
-    dt_object = datetime.fromtimestamp(ts)
+    dt_object = datetime.fromtimestamp(float(ts))
     local_tz_dt = dt_object.astimezone(LOCAL_TIMEZONE)
 
     short_str = local_tz_dt.strftime("%Y-%m-%d %H:%M:%S.%f")
