7ecde53fef526e020a33f07cedcfb4401c49158e
Bystroushaak <bystrousak@kitakitsune.org>
2022-02-19 22:23:22 +0100

    Charts are now working in the most basic mode.

--- src/result_obj_gui/result_obj_gui.py
+++ src/result_obj_gui/result_obj_gui.py
@@ -4,6 +4,9 @@ import argparse
 
 import justpy as jp
 
+from result_obj.metrics import Metric
+from result_obj.metrics import MetricInfo
+
 from utils import str_from_ts
 from utils import html_from_ts
 from utils import iso_str_from_ts
@@ -120,7 +123,155 @@ def _add_overview_section(div_content, db):
 def _add_metrics_section(div_content, db):
     section_metrics = _create_section(div_content, "Metrics")
 
+    cursor = db.cursor()
+    cursor.execute("SELECT DISTINCT name, type FROM Metrics")
+    metrics_list = cursor.fetchall()
+
+    if not metrics_list:
+        return None
+
+    def _read_metrics(metric_name, metric_type):
+        cursor = db.cursor()
+        if metric_type == Metric.TYPE_START:
+            cursor.execute(
+                "SELECT * FROM Metrics WHERE name=? AND (type=? OR type=?)",
+                (metric_name, Metric.TYPE_START, Metric.TYPE_STOP)
+            )
+        else:
+            cursor.execute(
+                "SELECT * FROM Metrics WHERE name=? AND type=?",
+                (metric_name, metric_type)
+            )
+
+        empty_dict = {}
+        for metric in cursor.fetchall():
+            m = MetricInfo(metric["type"], metric_name, empty_dict, metric["value"])
+            m.timestamp = metric["timestamp"]
+            yield m
+
+    metrics_start_stop = {}
+    metrics_counters = {}
+    metrics_value = {}
+    for metric in metrics_list:
+        metric_name = metric["name"]
+        metric_type = metric["type"]
+
+        metrics_generator = _read_metrics(metric_name, metric_type)
+        if metric_type == Metric.TYPE_START:
+            metrics_start_stop[metric_name] = metrics_generator
+        if metric_type == Metric.TYPE_INCREMENT:
+            metrics_counters[metric_name] = metrics_generator
+        if metric_type == Metric.TYPE_VALUE:
+            metrics_value[metric_name] = metrics_generator
+
+    for metric_name, metric_data in metrics_start_stop.items():
+        _add_chart_start_stop(metric_data, metric_name, section_metrics)
+    for metric_name, metric_data in metrics_counters.items():
+        _add_chart_counter(metric_data, metric_name, section_metrics)
+    for metric_name, metric_data in metrics_value.items():
+        _add_chart_values(metric_data, metric_name, section_metrics)
+
     return section_metrics
+
+
+def _add_chart_start_stop(metric_data, metric_name, section_metrics):
+    y_axis = []
+    x_axis = []
+    for metric_info in sorted(metric_data, key=lambda x: x.timestamp):
+        # make zero the default
+        y_axis.append(metric_info.timestamp * 1000 - 1)
+        x_axis.append(0)
+
+        y_axis.append(metric_info.timestamp * 1000)
+
+        if metric_info.type == Metric.TYPE_START:
+            x_axis.append(metric_info.timestamp)
+        else:
+            time_diff = metric_info.timestamp - x_axis[-1]
+            x_axis[-1] = time_diff
+            x_axis.append(time_diff)
+
+            # default back to zero
+            y_axis.append(metric_info.timestamp * 1000 - 1)
+            x_axis.append(0)
+
+    my_chart_def = {
+        "title": {
+            "text": metric_name
+        },
+        "xAxis": {
+            "type": 'datetime'
+        },
+        "yAxis": {
+            "title": {
+                "text": 'Start/Stop'
+            }
+        },
+        "series": [{
+            "name": 'Value',
+            "data": list(zip(y_axis, x_axis))
+        }]
+    }
+    my_chart = jp.HighCharts(a=section_metrics, classes='m-2 p-2 border')
+    my_chart.options = my_chart_def
+
+
+def _add_chart_counter(metric_data, metric_name, section_metrics):
+    y_axis = []
+    x_axis = []
+    counter = 0
+    for metric_info in metric_data:
+        y_axis.append(metric_info.timestamp * 1000)
+        x_axis.append(counter)
+        counter += 1
+
+    my_chart_def = {
+        "title": {
+            "text": metric_name
+        },
+        "xAxis": {
+            "type": 'datetime'
+        },
+        "yAxis": {
+            "title": {
+                "text": 'Value'
+            }
+        },
+        "series": [{
+            "name": 'Value',
+            "data": list(zip(y_axis, x_axis))
+        }]
+    }
+    my_chart = jp.HighCharts(a=section_metrics, classes='m-2 p-2 border')
+    my_chart.options = my_chart_def
+
+
+def _add_chart_values(metric_data, metric_name, section_metrics):
+    y_axis = []
+    x_axis = []
+    for metric_info in metric_data:
+        y_axis.append(metric_info.timestamp * 1000)
+        x_axis.append(metric_info.value)
+
+    my_chart_def = {
+        "title": {
+            "text": metric_name
+        },
+        "xAxis": {
+            "type": 'datetime'
+        },
+        "yAxis": {
+            "title": {
+                "text": 'Hits'
+            }
+        },
+        "series": [{
+            "name": 'counter',
+            "data": list(zip(y_axis, x_axis))
+        }]
+    }
+    my_chart = jp.HighCharts(a=section_metrics, classes='m-2 p-2 border')
+    my_chart.options = my_chart_def
 
 
 def _add_status_section(div_content, db):
